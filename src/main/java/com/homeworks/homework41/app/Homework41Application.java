c47ac02ce27f56e309ebec8ecadf753c9c9d411d
Kyrylo Kusovsky <kkirilll2006@gmail.com>
2025-02-15 12:50:53 +0200

    initial commit

---
+++ src/main/java/com/homeworks/homework41/app/Homework41Application.java
@@ -0,0 +1,13 @@
+package com.homeworks.homework41.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework41Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homework41Application.class, args);
+    }
+
+}
